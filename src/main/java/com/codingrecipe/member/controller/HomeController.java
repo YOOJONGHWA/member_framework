2cc927008f5eb1ce0fac304279acac7b2b415f16
YOOJONGHWA <sola120731@gmail.com>
2023-12-29 00:57:15 +0900

    개념정리

--- src/main/java/com/codingrecipe/member/controller/HomeController.java
+++ src/main/java/com/codingrecipe/member/controller/HomeController.java
@@ -3,10 +3,10 @@ package com.codingrecipe.member.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@Controller
+@Controller     // 스프링이 @Controller 어노테이션을 확인해서 구현해준다.
 public class HomeController {
     @GetMapping("/")
     public String index() {
-        return "index";
+        return "index"; // jsp 파일 이름이라 생각하면 됨
     }
 }
