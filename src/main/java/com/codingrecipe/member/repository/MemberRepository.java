c8dde6be1f96be89efd65b4e1b00131c435400bb
YOOJONGHWA <sola120731@gmail.com>
2023-12-29 18:09:10 +0900

    디비보안 테스트

--- src/main/java/com/codingrecipe/member/repository/MemberRepository.java
+++ src/main/java/com/codingrecipe/member/repository/MemberRepository.java
@@ -1,13 +1,17 @@
 package com.codingrecipe.member.repository;
 
 import com.codingrecipe.member.dto.MemberDTO;
+import lombok.RequiredArgsConstructor;
+import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.stereotype.Repository;
 
 @Repository
+@RequiredArgsConstructor
 public class MemberRepository {
+    private  final SqlSessionTemplate sql;
 
     public int save(MemberDTO memberDTO) {
         System.out.println("memberDTO = " + memberDTO);
-        return 0;
+        return sql.insert("Member.save", memberDTO);
     }
 }
