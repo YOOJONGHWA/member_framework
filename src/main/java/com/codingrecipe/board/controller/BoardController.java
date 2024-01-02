8d18b623225814a0ddc511bd7be02362ffb8fb8d
YOOJONGHWA <sola120731@gmail.com>
2024-01-02 23:06:45 +0900

    게시판 기능과 합치는 과정중

---
+++ src/main/java/com/codingrecipe/board/controller/BoardController.java
@@ -0,0 +1,86 @@
+package com.codingrecipe.board.controller;
+
+import com.codingrecipe.board.dto.BoardDTO;
+import com.codingrecipe.board.service.BoardService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/board")
+@RequiredArgsConstructor
+public class BoardController {
+
+    private final BoardService boardService;
+    @GetMapping("/index")
+    public String index() {
+        return "board/index";
+    }
+
+    @GetMapping("/save")
+    public String saveForm() {
+        return "board/save";
+    }
+
+    @PostMapping("/save")
+    public String save(@ModelAttribute BoardDTO boardDTO) {
+        int saveResult = boardService.save(boardDTO);
+        if(saveResult > 0) {
+            return "redirect:/board/list";
+        }
+        else {
+            return "board/save";
+        }
+    }
+
+    @GetMapping("/list")
+    public String findAll(Model model) {
+        List<BoardDTO> boardDTOList = boardService.findAll();
+        model.addAttribute("boardList",boardDTOList);
+        return "board/list";
+    }
+
+    @GetMapping
+    public String findById(@RequestParam("id") Long id, Model model) {
+        boardService.updateHits(id);
+        BoardDTO boardDTO = boardService.findAById(id);
+        model.addAttribute("board",boardDTO);
+        return "board/detail";
+    }
+
+    @GetMapping("/delete")
+    public String delete(@RequestParam("id") long id) {
+        boardService.delete(id);
+        return "redirect:/board/list";
+    }
+
+    @GetMapping("/update")
+    public String updateForm(@RequestParam("id") long id, Model model) {
+        BoardDTO boardDTO = boardService.findAById(id);
+        model.addAttribute("board",boardDTO);
+        return "board/update";
+    }
+
+    @PostMapping("/update")
+    public String update(@ModelAttribute BoardDTO boardDTO, Model model) {
+        boardService.update(boardDTO);
+        BoardDTO dto = boardService.findAById(boardDTO.getId());
+        model.addAttribute("board",dto);
+        return "board/detail";
+    }
+
+    // /board/paging?page=2
+    // 처음 페이지 요청은 1페이지를 보여줌
+    @GetMapping("/paging")
+    public String paging(Model model,
+                         @RequestParam(value = "page", required = false, defaultValue = "1") int page) {
+        System.out.println(" page = " + page);
+        // 해당 페이지에서 보여줄 글 목록
+        List<BoardDTO> pagingList = boardService.pagingList(page);
+        return "board/index";
+    }
+
+}
